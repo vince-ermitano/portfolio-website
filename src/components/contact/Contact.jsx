5b6e6223751652f3641f242cf9f246b62e0d9796
vince-ermitano <93059468+vince-ermitano@users.noreply.github.com>
2022-05-21 22:24:07 -0700

    Finish footer section comp

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineMail} from 'react-icons/md'
+import {RiMessengerFill} from 'react-icons/ri'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_bp5ncjq', 'template_qg4g2uj', form.current, 'g_pCeElPc632TK0As');
+
+    e.target.reset();
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineMail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>vermitan@ucsd.edu</h5>
+            <a href="mailto:vermitan@ucsd.edu" target="_blank">Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerFill className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Vince Ermitano</h5>
+            <a href="https://m.me/vincegilbert.ermitano" target="_blank">Send a message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" placeholder='Your message' rows="7" required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
